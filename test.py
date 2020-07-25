cec46e916dc6ededbf62d0331fa335f3c5b0aafa
mac12m99 <m.toma99@gmail.com>
2020-07-25 14:49:50 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,10 @@
 import unittest
-import HtmlTestRunner
-
 
 class t(unittest.TestCase):
     def test_a(self):
         self.assertTrue(True)
     def test_b(self):
-        self.assertTrue(True)
+        self.assertTrue(False)
 
 if __name__ == '__main__':
-    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(report_name='report', add_timestamp=False,
-                                                           combine_reports=True))
+    unittest.main()
